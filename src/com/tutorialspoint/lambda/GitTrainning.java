d02a8d62f1f8aa6fed3eb8bd36c7847dfe8f2ad3
Riardo <riccar.rcs@hotmail.com>
2019-07-14 16:45:17 -0300

    Segundo Commit

--- src/com/tutorialspoint/lambda/GitTrainning.java
+++ src/com/tutorialspoint/lambda/GitTrainning.java
@@ -1,10 +1,13 @@
 package com.tutorialspoint.lambda;
 
+import java.util.Date;
+
 public class GitTrainning {
 	
 	public static void main(String[] args) {
 		
 		System.out.println("Alfa");
+		System.out.println(new Date().getTime());
 		
 	}
 }
